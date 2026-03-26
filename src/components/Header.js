c719605da849496d0e464a8ec626f46979f8849b
unknown <faeze.danesh@yahoo.com>
2026-03-26 10:53:27 -0300

    Add meta tags and Open Graph setup for Little Lemon

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../icons_assets/Logo.svg'
+
+
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon logo" />
+    </header>
+  )
+}
+
+export default Header
